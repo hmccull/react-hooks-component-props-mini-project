c63c547aee8b4b6e4cf51d466f5c4b70dd3ffbfc
Hannah McCullough <hmac091@gmail.com>
2021-12-14 15:28:24 -0700

    commit tree 3e92b6238cec534b1c448573bd6f64a168b42507

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,11 +1,29 @@
-function Article({ title, date='January 1, 1970', preview }) {
+function createEmoji(minutes) {
+    const duration = minutes < 30 ? 5 : 10;
+    const emoji = minutes < 30 ? '☕️' : '🍱';
+
+    let emojis = '';
+
+    for (let i = 0; i < minutes; i += duration) {
+        emojis += emoji;
+    } 
+    
+    return emojis;
+}
+
+function Article({ title, date='January 1, 1970', preview, minutes }) {
+
+    const emojis = createEmoji(minutes);
+
     return (
         <article>
             <h3>{title}</h3>
-            <small>{date}</small>
+            <small>
+                {date} • {emojis} {minutes} min read
+            </small>
             <p>{preview}</p>
         </article>
-    )
+    );
 }
 
 export default Article;
